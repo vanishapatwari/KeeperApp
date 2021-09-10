acf2573d34874d3b543a016bfb2c6552c5082537
unknown <ipriyanshugautam@gmail.com>
2021-09-10 22:48:06 +0530

    Updates

---
+++ precache-manifest.4c6294914abaaf6df5dd7f2399f69e68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcdae987c085cbbfdb5a2194ee3062e",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/KeeperApp/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "3347237eab413b6d7086",
+    "url": "/KeeperApp/static/js/main.46d5cf3c.chunk.js"
+  },
+  {
+    "revision": "f87f7f290d4c0085841d",
+    "url": "/KeeperApp/static/js/runtime-main.9f8bc1f0.js"
+  }
+]);
